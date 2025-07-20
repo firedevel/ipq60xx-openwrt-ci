9336b8bfa4aa0fc835920ed2f7eb5f40a872ffb3
Your Name <you@example.com>
2025-07-20 23:29:26 +0800

    First

---
+++ diy.sh
@@ -0,0 +1,7 @@
+## LibWrt
+rm -rf package/emortal/luci-app-athena-led
+git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-
+
+## 替换软件源
+# To be done.
